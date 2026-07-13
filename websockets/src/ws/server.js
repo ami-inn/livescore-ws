e30b63bae87e8bf73b8f075a48e6dad6a2d181e2
ami <ami@amis-MacBook-Air.local>
2026-07-13 07:01:57 -0700

    update: modfied the re conection logic on the server js

--- websockets/src/ws/server.js
+++ websockets/src/ws/server.js
@@ -45,8 +45,28 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on("connection", (socket) => {
+    (/** @type {{ isAlive?: boolean }} */ (socket)).isAlive = true; // Mark the socket as alive when a new connection is established
+    //on connection we are attaching isAlive property to the socket object to keep track of the connection status
+    socket.on('pong', () => {
+      /** @type {{ isAlive?: boolean }} */ (socket).isAlive = true; // Mark the socket as alive when a pong is received
+    });
     sendJson(socket, { type:'welcome'});
+    socket.on('error',console.error);
     console.log("New WebSocket connection established");
+  });
+
+  // iterate over all connected clients every 30 seconds and check if they are alive, if not terminate the connection
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+       // cast ws to include isAlive property to satisfy typing
+       if ((/** @type {{ isAlive?: boolean }} */ (ws)).isAlive === false) return ws.terminate(); // Terminate the connection if the client is not alive
+       (/** @type {{ isAlive?: boolean }} */ (ws)).isAlive = false;
+       ws.ping(); // Send a ping to the client to check if it's alive
+    });
+  }, 30000);
+
+  wss.on("close", () => {
+    clearInterval(interval); // Clear the interval when the WebSocket server is closed
   });
 
   /**
